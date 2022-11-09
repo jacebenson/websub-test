c13dbd397e8eda70380c3c117f0b4f82de58e8ee
Jace Benson <638764+jacebenson@users.noreply.github.com>
2022-11-08 19:20:58 -0600

    ..

--- netlify/functions/websub.js
+++ netlify/functions/websub.js
@@ -1,9 +1,10 @@
 import Parser from 'rss-parser'
+var CryptoJS = require('crypto-js');
 export const handler = async (event, context) => {
   // feed example sn dev yt https://www.reddit.com/.rss
   // feed hub https://pubsubhubbub.appspot.com/subscribe
-  console.log('event', event)
-  console.log('event', context)
+  console.log('event.headers', event.headers)
+  //console.log('event', context)
   var mode = event.queryStringParameters['hub.mode']
   var topic = event.queryStringParameters['hub.topic']
   var challenge = event.queryStringParameters['hub.challenge'];
@@ -16,11 +17,13 @@ export const handler = async (event, context) => {
     challenge,
     leaseSeconds,
     secret,
-    timeNow: new Date().toLocaleTimeString('en-CA')
+    timeNow: new Date().toLocaleTimeString('en-CA'),
     //body
   })
   try {
     if (body) {
+      var checkedHash = CryptoJS.HmacSHA1(body, 'jace-secret').toString();
+      console.log('checked hash', checkedHash)
       let parser = new Parser({
         requestOptions: {
           rejectUnauthorized: false,
@@ -31,8 +34,8 @@ export const handler = async (event, context) => {
       console.log(parsedBody.feedUrl)
       parsedBody?.items.forEach(function(item){
         console.log('item.title', item.title)
-        console.log('  item.link', item.link)
-        console.log('  item.author', item.author)
+        //console.log('  item.link', item.link)
+        //console.log('  item.author', item.author)
         var date = new Date(item.isoDate).toLocaleDateString();
         var time = new Date(item.isoDate).toLocaleTimeString('en-CA')
         console.log('  item.isoDate', date, time)
@@ -48,3 +51,5 @@ export const handler = async (event, context) => {
   }
   return returnObj;
 }
+//sha1=6ddae16c27e4ce86f2be5ca1af30e1c0f893c26c //oreo pulls twitter ads
+//sha1=5c3146b93b5bf394c46689784ed5e75ad6480e9f //poll workers seen where maga poll station
